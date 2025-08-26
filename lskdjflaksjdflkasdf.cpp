cde2ff5ca39878d8bd6fa7e4f7c19e057f37492f
AMlhd <amlhdsan@gmail.com>
2025-08-26 23:11:42 +0800

    upd

--- lskdjflaksjdflkasdf.cpp
+++ lskdjflaksjdflkasdf.cpp
@@ -1,35 +1,39 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
+using ll=long long;
+#define IOS ios::sync_with_stdio(0);cin.tie(0)
+#define fr(i,l,r) for(int i=(l);i<=(r);++i)
+int main(){
+    IOS;
+    int T; if(!(cin>>T)) return 0;
+    while(T--){
+        int n; cin>>n;
+        vector<ll>a(n+2,0);
+        fr(i,1,n) cin>>a[i];
+        ll ops=0;
+        vector<ll> p(n+2,0);
+        ll mx = LLONG_MIN/4;
+        fr(i,1,n){
+            if(i>=2) mx = max(mx, p[i-2]);
+            int s = (i%2==0)?1:-1;
+            p[i] = p[i-1] + s * a[i];
+            if(p[i] < mx){
+                ll need = mx - p[i];
+                if(i%2==1){
+                    ll dec = min(need, a[i]);
+                    a[i] -= dec;
+                    ops += dec;
+                    p[i] += dec;
+                } else {
+                    ll dec = min(need, a[i-1]);
+                    a[i-1] -= dec;
+                    ops += dec;
+                    p[i-1] += dec;
+                    p[i] += dec;
+                }
+            }
+        }
+        cout<<ops<<"\n";
     }
-    while (ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return x * f;
-}
-
-inline void write(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
-}
-
-inline void writeln(int x) {
-    write(x);
-    putchar('\n');
-}
-
-int main() {
-
     return 0;
 }
