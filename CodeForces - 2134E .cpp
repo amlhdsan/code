ff025a2d4357bb45793cbbbec9f3b51ef7e9629c
AMlhd <amlhdsan@gmail.com>
2025-08-27 14:18:35 +0800

    upd

--- CodeForces - 2134E .cpp
+++ CodeForces - 2134E .cpp
@@ -1,86 +1,96 @@
 #include <bits/stdc++.h>
+
+#define N 100001
+
 using namespace std;
 
 inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return x * f;
+   int x = 0, f = 1;
+   char ch = getchar();
+   while (ch < '0' || ch > '9') {
+       if (ch == '-') f = -1;
+       ch = getchar();
+   }
+   while (ch >= '0' && ch <= '9') {
+       x = (x << 3) + (x << 1) + (ch ^ 48);
+       ch = getchar();
+   }
+   return x * f;
 }
 
 inline void write(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
+   if (x < 0) {
+       putchar('-');
+       x = -x;
+   }
+   if (x > 9) write(x / 10);
+   putchar(x % 10 + '0');
 }
 
 inline void writeln(int x) {
-    write(x);
-    putchar('\n');
+   write(x);
+   putchar('\n');
 }
 
-int a[200005], d[200005];
-int n, t;
+int t, n;
+int a[N];
+int jmp[N];
 
-void Swap(int x) {
-    printf("swap %d\n", x + 1);
-    fflush(stdout);
+void swp(int x) {
+   printf("swap %d\n", x + 1);
 }
 
-int Throw(int x) {
-    printf("throw %d\n", x + 1);
-    fflush(stdout);
-    int ret = read();
-    return ret;
+int thrw(int x) {
+   printf("throw %d\n", x + 1);
+   return read();
 }
 
-void Answer() {
-    putchar('!');
-    for(int i = 0; i < n; i++) {
-        putchar(' ');
-        write(a[i]);
-    }
-    putchar('\n');
-    fflush(stdout);
+void ans() {
+   putchar('!');
+   for(int i = 0; i < n; ++i) {
+       putchar(' ');
+       write(a[i]);
+   }
+   putchar('\n');
 }
 
 int main() {
-    t = read();
-    while (t--) {
-        n = read();
-        for (int i = 0; i < n; i++) a[i] = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (d[i + 1] == d[i + 2]) {
-                d[i] = d[i + 1] + 1;
-            } else {
-                d[i] = Throw(i);
-                if (d[i] == d[i + 1] + 1) a[i] = 1;
-                else a[i] = 2;
-            }
-        }
-        for (int i = 0; i + 1 < n; i++) {
-            if (a[i] == 0) {
-                Swap(i);
-                int j = Throw(i + 1);
-                if (j == d[i + 2] + 1) a[i] = 1;
-                else a[i] = 2;
-            }
-        }
-        Swap(n - 2);
-        int j = Throw(n - 2);
-        if (j == 2) a[n - 1] = 1;
-        else a[n - 1] = 2;
-        Answer();
-    }
-    return 0;
+   t = read();
+   while(t--) {
+       n = read();
+       
+       for(int i = n - 1; i >= 0; --i) {
+           if(jmp[i + 1] == jmp[i + 2]) {
+               jmp[i] = jmp[i + 1] + 1;
+           } else {
+               jmp[i] = thrw(i);
+               if(jmp[i] == jmp[i + 1] + 1)
+                   a[i] = 1;
+               else
+                   a[i] = 2;
+           }
+       }
+       
+       for(int i = 0; i + 1 < n; ++i) {
+           if(a[i] == 0) {
+               swp(i);
+               int ji = thrw(i + 1);
+               if(ji == jmp[i + 2] + 1)
+                   a[i] = 1;
+               else
+                   a[i] = 2;
+           }
+       }
+       
+       swp(n - 2);
+       int jlast = thrw(n - 2);
+       if(jlast == 2)
+           a[n - 1] = 1;
+       else
+           a[n - 1] = 2;
+           
+       ans();
+   }
+   
+   return 0;
 }
