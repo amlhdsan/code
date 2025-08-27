aae129b02c26b7a184f8b52d5e6100f177fb53b5
AMlhd <amlhdsan@gmail.com>
2025-08-27 21:33:57 +0800

    upd

---
+++ lskdjflksdjflkasdjf.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+int main() {
+    int P = read();
+    int T = read();
+    
+    int ans = (P + 1) / 2;
+    
+    while (T--) {
+        int n = read();
+        int m = read();
+        writeln(ans);
+    }
+    
+    return 0;
+}
--- usingnamespace.cpp
+++ usingnamespace.cpp
@@ -1,94 +1,72 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
 
-inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
+// 函数 f(s): 计算一个字符串 s 中长度为奇数的回文子串的数量
+// 使用“中心扩展法”实现
+// 时间复杂度: O(L^2), L 是字符串 s 的长度
+long long calculate_f(const std::string& s) {
+    long long count = 0;
+    int len = s.length();
+
+    // 遍历字符串中的每一个字符，将其作为回文中心
+    for (int center = 0; center < len; ++center) {
+        // 从当前中心向两边扩展
+        int left = center;
+        int right = center;
+        
+        // 只要还在字符串范围内，并且左右两边字符相同，就是一个回文串
+        while (left >= 0 && right < len && s[left] == s[right]) {
+            count++; // 发现一个奇数长度的回文子串
+            
+            // 继续向外扩展
+            left--;
+            right++;
+        }
     }
-    while (ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return x * f;
+    
+    return count;
 }
 
-inline void write(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
+// 处理单个测试用例的函数
+void solve() {
+    int n, m;
+    std::cin >> n >> m;
+    
+    // 计算 S 列表中所有字符串的 f 值总和
+    long long total_f_S = 0;
+    for (int i = 0; i < n; ++i) {
+        std::string s_i;
+        std::cin >> s_i;
+        total_f_S += calculate_f(s_i);
     }
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
-}
-
-inline void writeln(int x) {
-    write(x);
-    putchar('\n');
-}
-
-inline void writeLL(long long x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
+    
+    // 计算 T 列表中所有字符串的 f 值总和
+    long long total_f_T = 0;
+    for (int j = 0; j < m; ++j) {
+        std::string t_j;
+        std::cin >> t_j;
+        total_f_T += calculate_f(t_j);
     }
-    if (x > 9) writeLL(x / 10);
-    putchar(x % 10 + '0');
+    
+    // 根据推导出的公式计算最终答案
+    // 注意：n, m, total_f_S, total_f_T 的乘积可能很大，需要使用 long long 避免溢出
+    long long final_answer = (long long)m * total_f_S + (long long)n * total_f_T;
+    
+    std::cout << final_answer << "\n";
 }
 
 int main() {
-    int T = read();
+    // 加速 C++ 的输入输出
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int T;
+    std::cin >> T; // 读取测试数据组数
+    
     while (T--) {
-        int n = read();
-        int m = read();
-        vector<string> A(n), B(m);
-        for (int i = 0; i < n; ++i) {
-            static char buf[200005];
-            if(scanf("%s", buf)!=1) buf[0]=0;
-            A[i] = string(buf);
-        }
-        for (int j = 0; j < m; ++j) {
-            static char buf[200005];
-            if(scanf("%s", buf)!=1) buf[0]=0;
-            B[j] = string(buf);
-        }
-        long long ans = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                string s = A[i] + B[j];
-                int L = (int)s.size();
-                vector<int> d(L);
-                for (int k = 0; k < L; ++k) {
-                    int l = k - 1;
-                    int r = k + 1;
-                    int cnt = 1;
-                    while (l >= 0 && r < L && s[l] == s[r]) {
-                        --l; ++r; ++cnt;
-                    }
-                    d[k] = cnt;
-                    int a = k - 1, b = k + 1;
-                    int mirror = 1;
-                    while (a >= 0) {
-                        int needL = a - (d[a] - 1);
-                        int needR = b + (d[a] - 1);
-                        if (needL < 0 || needR >= L) break;
-                        bool ok = true;
-                        for (int p = 0; p < d[a]; ++p) {
-                            if (s[a - p] != s[b + p]) { ok = false; break; }
-                        }
-                        if (!ok) break;
-                        d[k] += d[a];
-                        a -= d[a];
-                        b += d[a];
-                    }
-                }
-                for (int v : d) ans += v;
-            }
-        }
-        writeLL(ans);
-        putchar('\n');
+        solve();
     }
+    
     return 0;
 }
