ef97b643cae10f51c9ef311177146f8b4f49f39c
AMlhd <amlhdsan@gmail.com>
2025-09-07 22:42:54 +0800

    upd

--- dlfkjsldkfj.cpp
+++ dlfkjsldkfj.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 inline int read() {
@@ -29,7 +30,31 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-int main() {
+void solve(int cur, int steps) {
+    if (steps == 0) {
+        writeln(cur);
+        return;
+    }
+    
+    solve(cur * 2, steps - 1);
+    
+    if ((cur - 1) % 3 == 0 && cur > 1) {
+        int prev = (cur - 1) / 3;
+        if (prev % 2 == 1) {
+            solve(prev, steps - 1);
+        }
+    }
+}
 
+int main() {
+    int t = read();
+    
+    while (t--) {
+        int k = read();
+        int x = read();
+        
+        solve(x, k);
+    }
+    
     return 0;
 }
