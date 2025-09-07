27e5c487c2a7209ab8ba578c45ca619448567fce
AMlhd <amlhdsan@gmail.com>
2025-09-07 22:47:03 +0800

    upd

--- lkjlkjf.cpp
+++ lkjlkjf.cpp
@@ -1,35 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return x * f;
-}
-
-inline void write(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
-}
-
-inline void writeln(int x) {
-    write(x);
-    putchar('\n');
-}
-
+using ll = long long;
 int main() {
-
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    if (!(cin >> T)) return 0;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> p(n+1);
+        for (int i = 1; i <= n; ++i) cin >> p[i];
+        vector<vector<int>> need(3), vals(3);
+        for (int i = 1; i <= n; ++i) {
+            int r = (3 - (p[i] % 3)) % 3;
+            need[r].push_back(i);
+        }
+        for (int v = 1; v <= n; ++v) vals[v % 3].push_back(v);
+        vector<int> q(n+1);
+        for (int r = 0; r < 3; ++r) {
+            for (size_t j = 0; j < need[r].size(); ++j) {
+                q[ need[r][j] ] = vals[r][j];
+            }
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (i > 1) cout << ' ';
+            cout << q[i];
+        }
+        cout << '\n';
+    }
     return 0;
 }
