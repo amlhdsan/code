36f1ce6c7b7e8a439cc88aa53181b4ace5dccbeb
AMlhd <amlhdsan@gmail.com>
2025-09-14 20:51:31 +0800

    upd

--- Luogu_P_3456.cpp
+++ Luogu_P_3456.cpp
@@ -40,13 +40,36 @@ int main() {
 
     n = read();
 
+    int maxxx = -1, minnn = 0x7fffffff;
+
     for(int i = 1; i <= n; ++i) {
         for(int j = 1; j <= n; ++j) {
             mp[i][j] = read();
+            maxxx = max(maxxx, mp[i][j]);
+            minnn = min(minnn, mp[i][j]);
             mp1[i][j] = mp[i][j];
             mp2[i][j] = mp[i][j];
         }
     }
+
+    if(maxxx == minnn) {
+        write(1);
+        putchar(' ');
+        write(1);
+        putchar('\n');
+        return 0;
+    }
+
+    // mp1 : 山峰(周围设置为山谷)
+
+    for(int i = 0; i <= n + 1; ++i) {
+        mp1[0][i] = -1;
+        mp1[n + 1][i] = -1;
+        mp1[i][0] = -1;
+        mp1[i][n + 1] = -1;
+    }
+
+
 
 
 
