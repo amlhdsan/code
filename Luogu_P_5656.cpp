e3b8d83276757acf0a8b306b3850ad941f2288bc
AMlhd <amlhdsan@gmail.com>
2025-09-16 23:15:57 +0800

    upd

--- Luogu_P_5656.cpp
+++ Luogu_P_5656.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 inline int read() {
     int x = 0, f = 1;
@@ -15,21 +16,44 @@ inline int read() {
     return x * f;
 }
 
-inline void write(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
+ll exgcd(ll a, ll b, ll &x, ll &y) {
+    if (!b) {
+        x = 1, y = 0;
+        return a;
     }
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
-}
-
-inline void writeln(int x) {
-    write(x);
-    putchar('\n');
+    ll d = exgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
 }
 
 int main() {
-
+    int T = read();
+    while (T--) {
+        ll a = read(), b = read(), c = read();
+        ll x, y;
+        ll d = exgcd(a, b, x, y);
+        if (c % d) {
+            puts("-1");
+            continue;
+        }
+        x *= c / d, y *= c / d;
+        ll tx = b / d, ty = a / d;
+        ll k1 = ceil((-x * 1.0) / tx);
+        ll k2 = floor((y * 1.0) / ty);
+        if (k1 > k2) {
+            ll k = ceil(-x * 1.0 / tx);
+            ll ansx = x + k * tx;
+            k = ceil(-y * 1.0 / ty);
+            ll ansy = y + k * ty;
+            printf("%lld %lld\n", ansx, ansy);
+            continue;
+        }
+        ll cnt = k2 - k1 + 1;
+        ll minx = x + k1 * tx;
+        ll miny = y - k1 * ty;
+        ll maxx = x + k2 * tx;
+        ll maxy = y - k2 * ty;
+        printf("%lld %lld %lld %lld %lld\n", cnt, minx, miny, maxx, maxy);
+    }
     return 0;
 }
