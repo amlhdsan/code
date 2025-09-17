a90a9eb972d2757f4f89b9d97fb36997f6b7eff6
AMlhd <amlhdsan@gmail.com>
2025-09-17 22:51:41 +0800

    upd

---
+++ Luogu_P_2303.cpp
@@ -0,0 +1,57 @@
+// 電影發明以後，人類的生命，比以前至少延長了三倍。
+// amlhdsan
+#include <bits/stdc++.h>
+using namespace std;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(long long x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(long long x) {
+    write(x);
+    putchar('\n');
+}
+
+long long phi(long long x) {
+    long long res = x;
+    for (long long i = 2; i * i <= x; i++) {
+        if (x % i == 0) {
+            res = res / i * (i - 1);
+            while (x % i == 0) x /= i;
+        }
+    }
+    if (x > 1) res = res / x * (x - 1);
+    return res;
+}
+
+int main() {
+    long long n = read();
+    long long ans = 0;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ans += i * phi(n / i);
+            if (i * i != n) ans += (n / i) * phi(i);
+        }
+    }
+    writeln(ans);
+    return 0;
+}
