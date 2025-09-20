ffe8fb5b6a02ccecbb9205790d13ba851b2841f8
AMlhd <amlhdsan@gmail.com>
2025-09-20 23:43:33 +0800

    upd

--- c.cpp
+++ c.cpp
@@ -1,76 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define N 1000100
-
-#define int long long
-
-int n, maxn, minn, top1, top2, ans;
-int a[N], Lb[N], Rb[N], Ls[N], Rs[N];
-int st1[N], st2[N];
-
-inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while(ch < '0' || ch > '9') {
-        if(ch == '-') {
-            f = -1;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    if (!(cin >> T)) return 0;
+    while (T--) {
+        int n;
+        string s;
+        cin >> n >> s;
+        bool ok = true;
+        for (int i = 1; i + 1 < n; ++i) {          // interior positions
+            if (s[i] == '0' && s[i-1] == '1' && s[i+1] == '1') {
+                ok = false;
+                break;
+            }
         }
-        ch = getchar();
+        cout << (ok ? "YES\n" : "NO\n");
     }
-    while(ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return x * f;
-}
-
-signed main() {
-    cin >> n;
-    for (int i = 1; i <= n; i++) 
-        a[i] = read();
-    top1 = 0, top2 = 0;
-    for(int i = 1; i <= n; i++) {
-        while(top1 && a[st1[top1]] > a[i]) 
-            top1--;
-        while(top2 && a[st2[top2]] < a[i]) 
-            top2--;
-        if(!top1) 
-            Ls[i] = 0;
-        else 
-            Ls[i] = st1[top1];
-        if(!top2) 
-            Lb[i] = 0;
-        else 
-            Lb[i] = st2[top2];
-        st1[++top1] = i;
-        st2[++top2] = i;
-    }
-    memset(st1, 0, sizeof(st1));
-    memset(st2, 0, sizeof(st2));
-    top1 = 0, top2 = 0;
-    for(int i = n; i >= 1; i--) {
-        while(top1 && a[st1[top1]] >= a[i]) 
-            top1--;
-        while(top2 && a[st2[top2]] <= a[i]) 
-            top2--;
-        if(!top1) 
-            Rs[i] = n+1;
-        else 
-            Rs[i] = st1[top1];
-        if(!top2) 
-            Rb[i] = n+1;
-        else 
-            Rb[i] = st2[top2];
-        st1[++top1] = i;
-        st2[++top2] = i;
-    }
-    for (int i = 1; i <= n; i++) {
-        ans += (i - Lb[i]) * (Rb[i] - i) * a[i];
-        ans -= (i - Ls[i]) * (Rs[i] - i) * a[i];
-    }
-
-    cout << ans;
-
     return 0;
 }
