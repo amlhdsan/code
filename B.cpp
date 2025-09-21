6bdba9ea11fd18455beaf572b10a9b51ae984e02
AMlhd <amlhdsan@gmail.com>
2025-09-21 10:35:57 +0800

    upd

--- B.cpp
+++ B.cpp
@@ -1,88 +1,41 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return x * f;
-}
-
-inline void write(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
-}
-
-inline void writeln(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) writeln(x / 10);
-    putchar(x % 10 + '0');
-    putchar('\n');
-}
-
-const int MAXN = 200005;
-int arr[MAXN];
-long long vals[MAXN];
-long long freq[MAXN];
-
-void printll(long long x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) printll(x / 10);
-    putchar(x % 10 + '0');
-}
+inline void 
 
 int main() {
-    int t = read();
+    int t;
+    cin >> t;
     while (t--) {
-        int n = read();
-        for (int i = 0; i < n; ++i) arr[i] = read();
-        sort(arr, arr + n);
-        int m = 0;
-        for (int i = 0; i < n; ) {
-            int j = i;
-            while (j < n && arr[j] == arr[i]) ++j;
-            vals[m] = arr[i];
-            freq[m] = j - i;
-            ++m;
-            i = j;
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
         }
+        sort(a.begin(), a.end());
+        long long total_moves = 0;
         long long alice = 0, bob = 0;
-        int turn = 0;
-        for (int i = 0; i < m; ++i) {
-            long long len = vals[i];
-            long long takeA = 0, takeB = 0;
-            if (turn == 0) {
-                takeA = (len + 1) / 2;
-                takeB = len / 2;
+        for (int i = 0; i < n; i++) {
+            long long f;
+            if (i == 0) {
+                f = a[i];
             } else {
-                takeB = (len + 1) / 2;
-                takeA = len / 2;
+                f = a[i] - a[i-1];
             }
-            alice += takeA * freq[i];
-            bob += takeB * freq[i];
-            if (len % 2 == 1) turn ^= 1;
+            long long s = n - i;
+            if (total_moves % 2 == 0) {
+                alice += (f + 1) / 2 * s;
+                bob += f / 2 * s;
+            } else {
+                alice += f / 2 * s;
+                bob += (f + 1) / 2 * s;
+            }
+            total_moves += f;
         }
-        printll(alice);
-        putchar(' ');
-        printll(bob);
-        putchar('\n');
+        cout << alice << " " << bob << "\n";
     }
     return 0;
 }
--- c.cpp
+++ c.cpp
@@ -1,23 +1,68 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int T;
-    if (!(cin >> T)) return 0;
-    while (T--) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
         int n;
         string s;
         cin >> n >> s;
-        bool ok = true;
-        for (int i = 1; i + 1 < n; ++i) {          // interior positions
-            if (s[i] == '0' && s[i-1] == '1' && s[i+1] == '1') {
-                ok = false;
-                break;
+        vector<pair<int, int>> segments;
+        int i = 0;
+        while (i < n) {
+            if (s[i] == '0') {
+                int start = i;
+                while (i < n && s[i] == '0') {
+                    i++;
+                }
+                int end = i - 1;
+                segments.push_back({start, end});
+            } else {
+                i++;
             }
         }
-        cout << (ok ? "YES\n" : "NO\n");
+        
+        bool valid = true;
+        for (auto seg : segments) {
+            int start = seg.first;
+            int end = seg.second;
+            int len = end - start + 1;
+            if (len >= 2) {
+                continue;
+            }
+            int idx = start;
+            bool left_flower = (idx > 0 && s[idx-1] == '1');
+            bool right_flower = (idx < n-1 && s[idx+1] == '1');
+            
+            if (left_flower && right_flower) {
+                if ((idx >= 2 && s[idx-2] == '0') || (idx + 2 < n && s[idx+2] == '0')) {
+                    // valid
+                } else {
+                    valid = false;
+                }
+            } else if (left_flower) {
+                if (idx >= 2 && s[idx-2] == '0') {
+                    // valid
+                } else {
+                    valid = false;
+                }
+            } else if (right_flower) {
+                if (idx + 2 < n && s[idx+2] == '0') {
+                    // valid
+                } else {
+                    valid = false;
+                }
+            }
+        }
+        
+        cout << (valid ? "YES" : "NO") << '\n';
     }
+    
     return 0;
 }
