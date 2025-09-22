c145b761692535e244f306fee321761fd7d7c6b9
AMlhd <amlhdsan@gmail.com>
2025-09-22 22:29:52 +0800

    upd

--- Luogu_CF_1017_F.cpp
+++ Luogu_CF_1017_F.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
 
-#define N 300000010
 #define int unsigned long long
-#define MOD (1 << 32)
+#define MOD (1ULL << 32)
 
 using namespace std;
 
 int n, a, b, c, d;
 int sum = 0;
-bool not_pri[N];
-int pri[200010], cnt = 0;
 
 inline int read() {
     int x = 0, f = 1;
@@ -26,7 +23,7 @@ inline int read() {
 }
 
 inline void write(int x) {
-    if (x < 0) {
+    if ((long long)x < 0) {
         putchar('-');
         x = -x;
     }
@@ -39,14 +36,17 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-inline void init() {
-    for(int i = 2; i <= n; ++i) {
-        if(!not_pri[i]) 
-            pri[++cnt] = i;
-        for(int j = 1; j <= cnt && i * pri[j] <= n; ++j) {
-            not_pri[i * pri[j]] = true;
-            if(i % pri[j] == 0) 
-                break;
+inline void init_sieve(int n, vector<int> &primes) {
+    vector<bool> is_comp(n + 1, false);
+    if (n >= 0) is_comp[0] = true;
+    if (n >= 1) is_comp[1] = true;
+    for (int i = 2; i <= (int)n; ++i) {
+        if (!is_comp[i]) primes.push_back(i);
+        for (size_t j = 0; j < primes.size(); ++j) {
+            unsigned long long prod = (unsigned long long)i * primes[j];
+            if (prod > (unsigned long long)n) break;
+            is_comp[(size_t)prod] = true;
+            if (i % primes[j] == 0) break;
         }
     }
 }
@@ -55,27 +55,31 @@ inline int f(int x) {
     return (a * x % MOD * x % MOD * x % MOD + b * x % MOD * x % MOD + c * x % MOD + d) % MOD;
 }
 
-inline void solve(int n, int a, int b, int c, int d) {
-    for(int i = 1; i <= cnt; ++i) {
-        int x = 0;
-        for(int j = pri[i]; j <= n; j *= pri[i]) 
+inline void solve(int n, int a, int b, int c, int d, const vector<int> &primes) {
+    for (size_t i = 0; i < primes.size(); ++i) {
+        int p = primes[i];
+        unsigned long long x = 0;
+        for (unsigned long long j = p; j <= (unsigned long long)n; j *= p) {
             x += n / j;
-        sum = (sum + f(pri[i]) * x % MOD) % MOD;
+            if (j > (unsigned long long)n / p) break; // 防止溢出
+        }
+        sum = (sum + f(p) * x % MOD) % MOD;
     }
     writeln(sum % MOD);
 }
 
 signed main() {
+    n = (int)read();
+    a = (int)read();
+    b = (int)read();
+    c = (int)read();
+    d = (int)read();
 
-    n = read();
-    a = read();
-    b = read();
-    c = read();
-    d = read();
+    vector<int> primes;
+    primes.reserve( (size_t)max(1u, (unsigned)min((unsigned)n, 100000u)) );
+    init_sieve((int)n, primes);
 
-    init();
-
-    solve(n, a, b, c, d);
+    solve(n, a, b, c, d, primes);
 
     return 0;
 }
