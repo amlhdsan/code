335d419d1eda1fe735dac03cc20dc4a0c33fbd80
AMlhd <amlhdsan@gmail.com>
2025-09-27 13:16:16 +0800

    upd

--- sldkjf.cpp
+++ sldkjf.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
+
+#define int long long
+
 using namespace std;
+
+int n;
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +34,43 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-int main() {
+inline int eulerphi(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+inline int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+signed main() {
+
+    int cnt = 0;
+
+    for(n = 1; n <= 10000; ++n) {
+        int x = 0, y = 0;
+
+        for(int i = 1; i <= n; ++i) {
+            x += gcd(i, n);
+            if(n % i == 0) 
+                y += i * eulerphi(i);
+        }
+
+        if(x * y >= n * n * n) {
+            cnt++;
+        }
+    }
+
+    writeln(cnt);
 
     return 0;
 }
