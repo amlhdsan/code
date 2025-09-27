a02d3355caec9fd5545320a33aad0d53209374a2
AMlhd <amlhdsan@gmail.com>
2025-09-27 22:03:22 +0800

    upd

--- CF893E.cpp
+++ CF893E.cpp
@@ -35,50 +35,21 @@ inline void writeln(int x) {
 inline void solve(int x, int y) {
     // x = p1^a1 * p2^a2 * ... * pk^ak
 
-    // ans = \sum C_{a1 + y - 1}^{y - 1}
-}
+    // ans = \sum C_{a1 + y - 1}^{y - 1} % MOD
 
-int main() {
-
-    int T = read();
-
-    while(T--) {
-        int x = read();
-        int y = read();
-        solve(x, y);
+    int ans = 1;
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0) {
+            int cnt = 0;
+            while (x % i == 0) {
+                x /= i;
+                cnt++;
+            }
+            ans = (ans * (cnt + y - 1)) % MOD;
+        }
     }
 
-    return 0;
-}
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return x * f;
-}
-
-inline void write(int x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
-}
-
-inline void writeln(int x) {
-    write(x);
-    putchar('\n');
-}
-
-inline void solve(int x, int y) {
-    // x = p1^a1 * p2^a2 * ... * pk^ak
-
-    // ans = \sum C_{a1 + y - 1}^{y - 1}
+    writeln(ans);
 }
 
 int main() {
