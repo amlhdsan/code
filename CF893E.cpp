3076ceec8962c9137327feca6815a63f1cdc12b2
AMlhd <amlhdsan@gmail.com>
2025-09-27 21:50:44 +0800

    upd

--- CF893E.cpp
+++ CF893E.cpp
@@ -1,11 +1,22 @@
 #include <bits/stdc++.h>
 
-#define 
+#define MOD 1000000007
 
 using namespace std;
 
 inline int read() {
     int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
     char ch = getchar();
     while (ch < '0' || ch > '9') {
         if (ch == '-') f = -1;
