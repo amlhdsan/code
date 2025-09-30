65b2b3dc55c8f935b5253ad1f1c4afac6eb89a97
AMlhd <amlhdsan@gmail.com>
2025-09-30 22:56:52 +0800

    upd

--- enlarge.cpp
+++ enlarge.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+int t[110], v[110];
+int dp[110][2610][110];
+
 inline int read() {
     int x = 0, f = 1;
     char ch = getchar();
@@ -30,6 +34,13 @@ inline void writeln(int x) {
 }
 
 int main() {
+
+    n = read();
+
+    for(int i = 1; i <= n; ++i) {
+        v[i] = read();
+        t[i] = read();
+    }
 
     return 0;
 }
