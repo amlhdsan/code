724c29d9e7bb296928b81ae436643f03220e1947
AMlhd <amlhdsan@gmail.com>
2025-10-04 10:39:51 +0800

    upd

--- Luogu_P_10953.cpp
+++ Luogu_P_10953.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
+
+#define N 1000010
+
 using namespace std;
+
+int n, m;
+int head[N], nxt[N], to[N], e = 1;
+int dfn[N], low[N], tot = 0;
+int iff[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +37,35 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
+inline void add_edge(int u, int v) {
+    nxt[++e] = head[u];
+    head[u] = e;
+    to[e] = v;
+}
+
+inline void tarjan(int p, int pre) {
+    dfn[p] = low[p] = ++tot;
+
+    for(int i = head[p]; i; i = nxt[i]) {
+        int v = to[i];
+
+
+    }
+}
+
 int main() {
+
+    n = read();
+    m = read();
+
+    for(int i = 1; i <= m; ++i) {
+        int u, v;
+        u = read();
+        v = read();
+        
+        add_edge(u, v);
+        add_edge(v, u);
+    }
 
     return 0;
 }
