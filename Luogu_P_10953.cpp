93abdd7fc52b30d750af2ccb7c1a64729c307758
AMlhd <amlhdsan@gmail.com>
2025-10-04 10:58:17 +0800

    upd

--- Luogu_P_10953.cpp
+++ Luogu_P_10953.cpp
@@ -1,13 +1,20 @@
 #include <bits/stdc++.h>
 
-#define N 1000010
+#define N 400010
+#define M 100010
 
 using namespace std;
 
 int n, m;
+int uu[N], vv[N];
 int head[N], nxt[N], to[N], e = 1;
 int dfn[N], low[N], tot = 0;
 int iff[N];
+stack<int> sk;
+int bl[N], sum = 0;
+
+vector<int> edges[M];
+int fa[M][20];
 
 inline int read() {
     int x = 0, f = 1;
@@ -45,12 +52,38 @@ inline void add_edge(int u, int v) {
 
 inline void tarjan(int p, int pre) {
     dfn[p] = low[p] = ++tot;
+    sk.push(p);
 
     for(int i = head[p]; i; i = nxt[i]) {
         int v = to[i];
 
+        if(v == pre) {
+            continue;
+        }
 
+        if(!dfn[v]) {
+            tarjan(v, p);
+            low[p] = min(low[p], low[v]);
+        }
+        else {
+            low[p] = min(low[p], dfn[v]);
+        }
     }
+
+    if(dfn[p] == low[p]) {
+        ++sum;
+        bl[p] = sum;
+        while(sk.top() != p) {
+            bl[sk.top()] = sum;
+            sk.pop();
+        }
+        sk.pop();
+    }
+}
+
+inline void addedge(int u, int v) {
+    edges[u].push_back(v);
+    edges[v].push_back(u);
 }
 
 int main() {
@@ -59,13 +92,26 @@ int main() {
     m = read();
 
     for(int i = 1; i <= m; ++i) {
-        int u, v;
-        u = read();
-        v = read();
+        uu[i] = read();
+        vv[i] = read();
         
-        add_edge(u, v);
-        add_edge(v, u);
+        add_edge(uu[i], vv[i]);
+        add_edge(vv[i], uu[i]);
     }
+
+    for(int i = 1; i <= n; ++i) {
+        if(!dfn[i]) {
+            tarjan(i, 0);
+        }
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        if(bl[vv[i]] != bl[uu[i]]) {
+            addedge(bl[uu[i]], bl[vv[i]]);
+        }
+    }
+
+    
 
     return 0;
 }
