8662bbd35c37fb12dfaded293e857a6e4c5e4bba
AMlhd <amlhdsan@gmail.com>
2025-10-05 16:33:06 +0800

    upd

--- Luogu_B_4016.cpp
+++ Luogu_B_4016.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
+
+#define N 200010
+
 using namespace std;
+
+int n;
+int head[N], nxt[N], to[N], e = 0;
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +35,23 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
+inline void add_edge(int u, int v) {
+    nxt[++e] = head[u];
+    head[u] = e;
+    to[e] = v;
+}
+
 int main() {
+
+    n = read();
+
+    for(int i = 1; i <= n - 1; ++i) {
+        int u, v;
+        u = read();
+        v = read();
+        add_edge(u, v);
+        add_edge(v, u);
+    }
 
     return 0;
 }
