a5fef2dca70df01a935170146d6f3a13b892e870
AMlhd <amlhdsan@gmail.com>
2025-10-06 22:07:15 +0800

    upd

--- Luogu_P_4652.cpp
+++ Luogu_P_4652.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
+
+#define N 400000
+
 using namespace std;
+
+int n, m;
+int nxt[N], head[N], to[N], e = 0;
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +35,34 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
+inline void add_edge(int u, int v) {
+    nxt[++e] = head[u];
+    head[u] = e;
+    to[e] = v;
+}
+
+inline void tarjan(int p) {
+    dfn[p] = low[p] = ++cnt;
+    sk.push(p);
+    iff[p] = 1;
+
+    for(int i = head[p]; i; i = nxt[i]) {
+        
+    }
+}
+
 int main() {
+
+    n = read();
+    m = read();
+
+    for(int i = 1; i <= m; ++i) {
+        int u, v;
+        u = read();
+        v = read();
+        add_edge(u, v);
+        add_edge(v, u);
+    }
 
     return 0;
 }
