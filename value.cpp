6bcf85595984134d7d9bd53d8d445e1f3e99b224
AMlhd <amlhdsan@gmail.com>
2025-10-07 21:58:14 +0800

    upd

--- value.cpp
+++ value.cpp
@@ -1,5 +1,14 @@
 #include <bits/stdc++.h>
+
+#define N 10000010
+#define int long long
+
 using namespace std;
+
+int n;
+int x, y, z;
+int a[N];
+int f[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +38,30 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-int main() {
+inline void gen() {
+    for(int i = 1; i <= n; ++i) {
+        a[i] = (a[i - 1] * x + y) % z;
+    }
+}
+
+inline void output() {
+    int ans = 0;
+    for(int i = 1; i <= n; ++i) {
+        ans = (ans * x + f[i] + y) % z;
+    }
+    writeln(ans);
+}
+
+signed main() {
+
+    n = read();
+    x = read(), y = read(), z = read();
+
+    gen();
+
+    
+
+    output();
 
     return 0;
 }
