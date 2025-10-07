50d48580797496707ab5fbbde3a75cb9b1838541
AMlhd <amlhdsan@gmail.com>
2025-10-07 22:47:58 +0800

    upd

--- value.cpp
+++ value.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
-
-#define N 10000010
-#define int long long
-
 using namespace std;
 
+const int MAXN = 4000005;
 int n;
-int x, y, z;
-int a[N];
-int f[N];
+int x,y,z;
+int *a;
+int *ans;
+int *st;
+int top = 0;
+int *L;
+int *R;
 
 inline int read() {
     int x = 0, f = 1;
@@ -38,32 +39,39 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-inline void gen() {
-    for(int i = 1; i <= n; ++i) {
-        a[i] = (a[i - 1] * x + y) % z;
-    }
-}
-
-inline void output() {
-    int ans = 0;
-    for(int i = 1; i <= n; ++i) {
-        ans = (ans * x + f[i] + y) % z;
-    }
-    writeln(ans);
-}
-
-signed main() {
-
+int main() {
     n = read();
-    x = read(), y = read(), z = read();
-
-    gen();
-
-    for(int i = 1; i <= n; ++i) {
-        
+    x = read(); y = read(); z = read();
+    a = (int*)malloc((n+5)*sizeof(int));
+    ans = (int*)malloc((n+5)*sizeof(int));
+    st = (int*)malloc((n+5)*sizeof(int));
+    L = (int*)malloc((n+5)*sizeof(int));
+    R = (int*)malloc((n+5)*sizeof(int));
+    a[0] = 0;
+    for (int i = 1; i <= n; ++i) a[i] = ( (long long)a[i-1]*x + y ) % z;
+    for (int i = 0; i <= n; ++i) ans[i] = INT_MAX;
+    top = 0;
+    for (int i = 1; i <= n; ++i) {
+        while (top>0 && a[st[top-1]] <= a[i]) --top;
+        L[i] = (top==0)?0:st[top-1];
+        st[top++] = i;
     }
-
-    output();
-
+    top = 0;
+    for (int i = n; i >= 1; --i) {
+        while (top>0 && a[st[top-1]] < a[i]) --top;
+        R[i] = (top==0)?(n+1):st[top-1];
+        st[top++] = i;
+    }
+    for (int i = 1; i <= n; ++i) {
+        int len = R[i] - L[i] - 1;
+        if (a[i] < ans[len]) ans[len] = a[i];
+    }
+    for (int i = n-1; i >= 1; --i) if (ans[i+1] < ans[i]) ans[i] = ans[i+1];
+    long long g = 0;
+    for (int i = 1; i <= n; ++i) {
+        int ai = ans[i];
+        g = (g * x + ai + y) % z;
+    }
+    writeln((int)g);
     return 0;
 }
