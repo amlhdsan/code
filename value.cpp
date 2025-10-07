3632ec54dad59bd97adb732422a98c93990b5188
AMlhd <amlhdsan@gmail.com>
2025-10-07 23:13:10 +0800

    upd

--- value.cpp
+++ value.cpp
@@ -1,15 +1,16 @@
+// 電影發明以後，人類的生命，比以前至少延長了三倍。
+// amlhdsan
 #include <bits/stdc++.h>
+
+#define int long long
+
 using namespace std;
 
-const int MAXN = 4000005;
-int n;
-int x,y,z;
-int *a;
-int *ans;
-int *st;
-int top = 0;
-int *L;
-int *R;
+const int N = 10000005;
+int n, x, y, z;
+int a[N], L[N], R[N], st[N];
+int ans[N];
+int top;
 
 inline int read() {
     int x = 0, f = 1;
@@ -39,39 +40,37 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-int main() {
+signed main() {
+
+    // freopen("value.in", "r", stdin);
+    // freopen("value.out", "w", stdout);
+
     n = read();
     x = read(); y = read(); z = read();
-    a = (int*)malloc((n+5)*sizeof(int));
-    ans = (int*)malloc((n+5)*sizeof(int));
-    st = (int*)malloc((n+5)*sizeof(int));
-    L = (int*)malloc((n+5)*sizeof(int));
-    R = (int*)malloc((n+5)*sizeof(int));
     a[0] = 0;
-    for (int i = 1; i <= n; ++i) a[i] = ( (long long)a[i-1]*x + y ) % z;
-    for (int i = 0; i <= n; ++i) ans[i] = INT_MAX;
+    for (int i = 1; i <= n; ++i) a[i] = ((long long)a[i - 1] * x + y) % z;
+    for (int i = 1; i <= n; ++i) ans[i] = 2147483647;
     top = 0;
     for (int i = 1; i <= n; ++i) {
-        while (top>0 && a[st[top-1]] <= a[i]) --top;
-        L[i] = (top==0)?0:st[top-1];
+        while (top > 0 && a[st[top - 1]] <= a[i]) --top;
+        L[i] = (top == 0 ? 0 : st[top - 1]);
         st[top++] = i;
     }
     top = 0;
     for (int i = n; i >= 1; --i) {
-        while (top>0 && a[st[top-1]] < a[i]) --top;
-        R[i] = (top==0)?(n+1):st[top-1];
+        while (top > 0 && a[st[top - 1]] < a[i]) --top;
+        R[i] = (top == 0 ? n + 1 : st[top - 1]);
         st[top++] = i;
     }
     for (int i = 1; i <= n; ++i) {
         int len = R[i] - L[i] - 1;
         if (a[i] < ans[len]) ans[len] = a[i];
     }
-    for (int i = n-1; i >= 1; --i) if (ans[i+1] < ans[i]) ans[i] = ans[i+1];
+    for (int i = n - 1; i >= 1; --i)
+        if (ans[i + 1] < ans[i]) ans[i] = ans[i + 1];
     long long g = 0;
-    for (int i = 1; i <= n; ++i) {
-        int ai = ans[i];
-        g = (g * x + ai + y) % z;
-    }
+    for (int i = 1; i <= n; ++i)
+        g = (g * x + ans[i] + y) % z;
     writeln((int)g);
     return 0;
 }
