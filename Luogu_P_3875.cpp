7fc95dd3cb4c996eb0ed71e15558c8397dc0ed0d
AMlhd <amlhdsan@gmail.com>
2025-10-13 23:07:43 +0800

    upd

--- Luogu_P_3875.cpp
+++ Luogu_P_3875.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 
 #define N 400010
+#define int long long
+#define ls (p << 1)
+#define rs (p << 1 | 1)
+#define mid ((l + r) >> 1)
 using namespace std;
 
 int n;
@@ -10,7 +14,12 @@ struct linee {
     int l, r;
     int h;
     int tag;
-}line;
+}line[N];
+
+struct treee {
+    int cnt;
+    int len;
+}tree[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -40,7 +49,25 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-int main() {
+inline bool cmp(linee x, linee y) {
+    return x.h < y.h;
+}
+
+inline void build(int p, int l, int r) {
+    tree[p].cnt = tree[p].len = 0;
+    if(l == r) {
+        return ;
+    }
+
+    build(ls, l, mid);
+    build(rs, mid + 1, r);
+}
+
+inline void mdf(int p, int l, int r, int ql, int qr, int x) {
+    
+}
+
+signed main() {
 
     n = read();
 
@@ -52,12 +79,23 @@ int main() {
         f = read();
         g = read();
         if(s == f) { // 平行于 y 轴
-            if(d > g) {
-                swap(d, g);
-            }
-            x1 = 
+            x1 = s - 1, x2 = s + 1;
+            y1 = min(d, g), y2 = max(d, g);
         }
+        else {
+            x1 = min(s, f), x2 = max(s, f);
+            y1 = d - 1, y2 = d + 1;
+        }
+        xx[++xxx] = x1, xx[++xxx] = x2;
+        line[i * 2 - 1] = {x1, x2, y1, 1};
+        line[i * 2] = {x1, x2, y2, -1};
     }
 
+    sort(xx + 1, xx + xxx + 1);
+
+    xxx = unique(xx + 1, xx + xxx + 1) - xx - 1;
+    sort(line + 1, line + 2 * n + 1, cmp);
+
+    build(1, 1, xxx - 1);
     return 0;
 }
