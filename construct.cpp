88515732a45e56f656537272f066b059bf911c5a
AMlhd <amlhdsan@gmail.com>
2025-10-26 20:32:52 +0800

    upd

--- construct.cpp
+++ construct.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
+
+#define MOD 1000000007
+
 using namespace std;
+
+int T;
+int n;
+int res;
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +36,33 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
+inline int qpow(int a, int b) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (1LL * res * a) % MOD;
+        a = (1LL * a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
 int main() {
+
+    T = read();
+    while(T--) {
+        n = read();
+        int k = qpow(2, n - 1);
+        res = 0;
+        for (int i = 1; i <= n; ++i) {
+            int l, r, x;
+            l = read();
+            r = read();
+            x = read();
+            res |= x;
+            res %= MOD;
+        }
+        writeln(res * k % MOD);
+    }
 
     return 0;
 }
