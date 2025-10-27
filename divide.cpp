3d23d60421e585f1faded4db75c65780c0093114
AMlhd <amlhdsan@gmail.com>
2025-10-27 22:28:22 +0800

    upd

--- divide.cpp
+++ divide.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
+
+#define N 100010
+#define K 21
+
 using namespace std;
+
+int n, k;
+int a[N];
+int dp[N][K];
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +37,27 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
+inline void wk(int k, int l, int r, int ql, int qr) {
+
+}
+
 int main() {
+
+    // freopen("divide.in", "r", stdin);
+    // freopen("divide.out", "w", stdout);
+
+    n = read();
+    k = read();
+
+    for(int i = 1; i <= n; ++i) {
+        a[i] = read();
+    }
+
+    dp[0][0] = 0;
+
+    for(int i = 1; i <= k; ++i) {
+        wk(i, 1, n, 0, n - 1);
+    }
 
     return 0;
 }
