e1d6511051f48dfc226cd987ae70454b5b8d8d38
AMlhd <amlhdsan@gmail.com>
2025-10-31 12:16:13 +0800

    upd

--- A. 【NOIP Round #8】位集 .cpp
+++ A. 【NOIP Round #8】位集 .cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
-
 #define N 100010
 #define ll long long
-
 using namespace std;
-
 inline int read() {
     int x = 0, f = 1;
     char ch = getchar();
@@ -18,7 +15,6 @@ inline int read() {
     }
     return x * f;
 }
-
 inline void write(ll x) {
     if (x < 0) {
         putchar('-');
@@ -27,79 +23,76 @@ inline void write(ll x) {
     if (x > 9) write(x / 10);
     putchar(x % 10 + '0');
 }
-
 inline void writeln(ll x) {
     write(x);
     putchar('\n');
 }
-
-int n, m, q;
-ll x, y, z;
-ll ans, las;
+int n, m, Q;
+ll X, Y, Z;
+ll Ans, las;
 int g[N];
 int st[N], tp;
-int a[N][25];
+int ans[N][25];
 int siz[N];
-bool mat[N][N];
-
+bool a[N][N];
 int main() {
     n = read();
     m = read();
-    
+
     for(int i = 1; i <= n; ++i) {
         static char s[N];
         scanf("%s", s + 1);
         for(int j = 1; j <= m; ++j) {
-            mat[i][j] = s[j] - '0';
+            a[i][j] = s[j] - '0';
         }
     }
-    
-    q = read();
-    x = read();
-    y = read();
-    z = read();
-    
+
+    Q = read();
+    X = read();
+    Y = read();
+    Z = read();
+
     for(int j = 1; j <= m; ++j) {
         g[j] = 1;
         st[++tp] = 1;
     }
-    
+
     sort(st + 1, st + tp + 1);
     tp = unique(st + 1, st + tp + 1) - st - 1;
-    
+
     for(int j = 1; j <= tp; ++j) {
-        a[1][j] = st[j];
+        ans[1][j] = st[j];
     }
     siz[1] = tp;
-    
+
     for(int i = 2; i <= n; ++i) {
         tp = 0;
         for(int j = 1; j <= m; ++j) {
-            if(mat[i][j] ^ mat[i - 1][j]) {
+            if(a[i][j] ^ a[i - 1][j]) {
                 g[j] = i;
             }
             st[++tp] = g[j];
         }
-        
+
         sort(st + 1, st + tp + 1);
         tp = unique(st + 1, st + tp + 1) - st - 1;
-        
+
         for(int j = 1; j <= tp; ++j) {
-            a[i][j] = st[j];
+            ans[i][j] = st[j];
         }
         siz[i] = tp;
     }
-    
-    int p = 1, q = n;
-    for(int i = 1; i <= q; ++i) {
-        int l = min(p, q), r = max(p, q);
-        las = upper_bound(a[r] + 1, a[r] + siz[r] + 1, l) - a[r] - 1;
-        p = (1ll * p * x + 1ll * las * y + z) % n + 1;
-        q = (1ll * q * y + 1ll * las * z + x) % n + 1;
-        ans += las;
+
+    int A = 1, B = n;
+    for(int i = 1; i <= Q; ++i) {
+        int l = min(A, B), r = max(A, B);
+        las = upper_bound(ans[r] + 1, ans[r] + siz[r] + 1, l) - ans[r] - 1;
+        A = (1ll * A * X + 1ll * las * Y + Z) % n + 1;
+        B = (1ll * B * Y + 1ll * las * Z + X) % n + 1;
+        Ans += las;
     }
-    
-    writeln(ans);
-    
+
+    writeln(Ans);
+
     return 0;
 }
