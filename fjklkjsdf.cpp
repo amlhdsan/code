971b63c5bb842de23c674f8f361f4009a652eb98
AMlhd <amlhdsan@gmail.com>
2025-10-31 22:33:20 +0800

    upd

---
+++ fjklkjsdf.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define N 1000010
+
+using namespace std;
+
+int n;
+int xx[N], xxx = 0;
+
+struct node {
+    int l, r;
+    int h;
+    int tag; 
+}line[N];
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+int main() {
+
+    n = read();
+
+    for(int i = 1; i <= n; ++i) {
+        int a1, b1, a2, b2;
+        a1 = read();
+        b1 = read();
+        a2 = read();
+        b2 = read();
+
+        xx[++xxx] = a1;
+        xx[++xxx] = a2;
+
+
+    }
+
+    return 0;
+}
