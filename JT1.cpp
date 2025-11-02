c763639d95f596a184adb2ec64089de5cde28ae3
AMlhd <amlhdsan@gmail.com>
2025-11-02 12:41:31 +0800

    udp

---
+++ JT1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+int n = 0;
+int k[10];
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+int main() {
+
+    cin >> str;
+
+    n = str.size();
+    str = " " + str;
+
+    for(int i = 1; i <= n; ++i) {
+        if(str[i] <= '9' && str[i] >= '0') {
+            
+        }
+    }
+
+    return 0;
+}
