17519094332a89f65f31cacf2b5eeb46e968bfb5
AMlhd <amlhdsan@gmail.com>
2025-11-02 12:48:27 +0800

    upd

--- JT2.cpp
+++ JT2.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int n, m;
 int a[N];
+int x;
 
 inline int read() {
     int x = 0, f = 1;
@@ -37,5 +38,27 @@ inline void writeln(int x) {
 
 int main() {
 
+    n = read();
+    m = read();
+
+    for(int i = 1; i <= n * m; ++i) {
+        a[i] = read();
+        a[i] = -a[i];
+    }
+    x = a[1];
+    sort(a + 1, a + 1 + n * m);
+    for(int i = 1; i <= n * m; ++i) {
+        a[i] = -a[i];
+    }
+    int p = lower_bound(a + 1, a + n * m + 1, p) - a - 1;
+    int t = p % m;
+    write(p / m + 1);
+    putchar(' ');
+    if(t & 1) { // ji
+        write(m - t + 1);
+    }
+    else {
+        write(t);
+    }
     return 0;
 }
