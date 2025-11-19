399654461ed7de347dddf35e5ac194061c9b1c7c
AMlhd <amlhdsan@gmail.com>
2025-11-19 22:07:40 +0800

    upd

--- Luogu_P_11362.cpp
+++ Luogu_P_11362.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int T;
 int n, m, v;
 
-int c[N];
+// int c[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -41,11 +41,15 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
+struct node {
+    int cc, d;
+}c[N];
+
 // v^{2x} - v^x + v^{x - 1}
 
 inline int qpow(int a, int b) {
     int res = 1;
-    while(b) {
+    while(b > 0) {
         if(b & 1) {
             res = res * a % MOD;
         }
@@ -60,7 +64,11 @@ inline void mul(int &x, int y) {
 }
 
 inline int f(int x) {
-    return (qpow(v, 2 * x) % MOD - qpow(v, x) % MOD + qpow(v, x - 1) % MOD) % MOD;
+    return (qpow(v, 2 * x) % MOD - qpow(v, x) % MOD + qpow(v, x - 1) % MOD + MOD) % MOD;
+}
+
+inline bool cmp(node x, node y) {
+    return x.cc < y.cc;
 }
 
 inline void work() {
@@ -71,23 +79,30 @@ inline void work() {
     int ans = 1;
 
     for(int i = 1; i <= m; ++i) {
-        c[i] = read();
-        read();
+        c[i].cc = read();
+        c[i].d = read();
     }
     
-    sort(c + 1, c + m + 1);
+    sort(c + 1, c + m + 1, cmp);
 
-    mul(ans, qpow(v, c[1] - 1));
+    mul(ans, qpow(v, 2 * c[1].cc - 2));
     for(int i = 2; i <= m; ++i) {
-        mul(ans, qpow(v, c[i] - c[i - 1] - 1));
+        if(c[i].cc == c[i - 1].cc && c[i].d != c[i - 1].d) {
+            writeln(0);
+            return;
+        }
+        else if(c[i].cc == c[i - 1].cc) {
+            continue;
+        }
+        mul(ans, f(c[i].cc - c[i - 1].cc));
     }
-    mul(ans, qpow(n - c[m] - 1));
+    mul(ans, qpow(v, 2 * n - c[m].cc * 2));
     writeln(ans);
 }
 
-int main() {
+signed main() {
 
-    // writeln(qpow(2, 10));
+    // writeln(qpow(2, 5));
 
     T = read();
 
