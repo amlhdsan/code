c285eeb97f0630fededa668831ca93070c6e17c9
AMlhd <amlhdsan@gmail.com>
2025-11-23 12:14:12 +0800

    upd

--- BBB.cpp
+++ BBB.cpp
@@ -1,179 +1,173 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+
+#define ll long long
+#define N 100005
+#define MOD 998244353
 
 using namespace std;
 
-typedef long long ll;
+int n;
+vector<int> adj[N], ch[N];
+int tin[N], tout[N], flat[N];
+int timer;
 
-const int MOD = 998244353;
-
-// 树状数组 (Fenwick Tree)
 struct BIT {
-    vector<int> tree;
-    int n;
-
-    BIT(int size) {
-        n = size;
-        tree.assign(n + 1, 0);
-    }
-
-    void add(int idx, int val) {
-        for (++idx; idx <= n; idx += idx & -idx) {
-            tree[idx] += val;
-        }
-    }
-
-    int query(int idx) {
-        int sum = 0;
-        for (++idx; idx > 0; idx -= idx & -idx) {
-            sum += tree[idx];
-        }
-        return sum;
+    int t[N], n;
+    
+    void init(int sz) {
+        n = sz;
+        memset(t, 0, sizeof(t));
     }
     
-    // 查询 [l, r] 区间
-    int query_range(int l, int r) {
-        return query(r) - query(l - 1);
+    void add(int x, int v) {
+        for(++x; x <= n; x += x & -x) {
+            t[x] += v;
+        }
     }
-
-    void clear() {
-        tree.assign(n + 1, 0);
+    
+    int qry(int x) {
+        int s = 0;
+        for(++x; x > 0; x -= x & -x) {
+            s += t[x];
+        }
+        return s;
     }
-};
+    
+    int qry(int l, int r) {
+        return qry(r) - qry(l - 1);
+    }
+} bit;
 
-// 快速幂
-ll power(ll base, ll exp) {
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(ll x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(ll x) {
+    write(x);
+    putchar('\n');
+}
+
+ll qpow(ll a, ll b) {
     ll res = 1;
-    base %= MOD;
-    while (exp > 0) {
-        if (exp % 2 == 1) res = (res * base) % MOD;
-        base = (base * base) % MOD;
-        exp /= 2;
+    a %= MOD;
+    while(b) {
+        if(b & 1) res = res * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
     }
     return res;
 }
 
-// 邻接表
-vector<vector<int>> adj_T;
-vector<vector<int>> children;
-vector<int> tin, tout, flat_tree;
-int timer;
-
-// DFS 构建树结构、DFS序、扁平树
-void dfs_build(int u, int p) {
+void dfs(int u, int p) {
     tin[u] = timer++;
-    flat_tree.push_back(u);
+    flat[tin[u]] = u;
     
-    // 排序孩子，以模拟 sort(adj[x].begin(), adj[x].end())
-    // 这一步是关键，我们必须基于给定的 T 建立父子关系
-    // 然后找出哪些孩子是“按顺序”访问的
-    // 不，我们不需要在这里排序。读入的 T 已经是 dfs 的结果
-    // 我们只需要建立 T 的树形结构
-    
-    for (int v : adj_T[u]) {
-        if (v != p) {
-            children[u].push_back(v);
-            dfs_build(v, u);
+    for(int v : adj[u]) {
+        if(v != p) {
+            ch[u].push_back(v);
+            dfs(v, u);
         }
     }
-    tout[u] = timer; // tout[u] 是子树结束后的时间戳
+    tout[u] = timer;
 }
 
-struct Query {
-    int id;
-    int val; // 对应的 c 值
+struct Qry {
+    int id, val;
 };
 
+vector<Qry> qs[N], qe[N];
+ll ans[N];
+
 void solve() {
-    int n;
-    cin >> n;
-
-    // 初始化/清空
-    adj_T.assign(n, vector<int>());
-    children.assign(n, vector<int>());
-    tin.assign(n, 0);
-    tout.assign(n, 0);
-    flat_tree.clear();
+    n = read();
+    
+    for(int i = 0; i < n; ++i) {
+        adj[i].clear();
+        ch[i].clear();
+        qs[i].clear();
+        qe[i].clear();
+    }
     timer = 0;
-
-    for (int i = 0; i < n - 1; ++i) {
-        int u, v;
-        cin >> u >> v;
-        adj_T[u].push_back(v);
-        adj_T[v].push_back(u);
+    
+    for(int i = 0; i < n - 1; ++i) {
+        int u = read(), v = read();
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-
-    // 1. 构建以 0 为根的树，并获取 DFS 序
-    dfs_build(0, -1);
-
-    // 2. 创建查询
-    vector<vector<Query>> queries_start(n);
-    vector<vector<Query>> queries_end(n);
-    vector<ll> answers;
-    int query_id = 0;
-
-    for (int u = 0; u < n; ++u) {
-        // 关键：T 是 dfs 的结果，而 dfs 访问孩子是按编号排序的
-        // 所以我们必须对 T 的孩子列表排序，才能正确模拟
-        sort(children[u].begin(), children[u].end());
-
-        for (int c : children[u]) {
-            // 查询：c 的子树中（[tin[c], tout[c]-1]），有多少 v > c
+    
+    dfs(0, -1);
+    
+    int qid = 0;
+    
+    for(int u = 0; u < n; ++u) {
+        sort(ch[u].begin(), ch[u].end());
+        
+        for(int c : ch[u]) {
             int l = tin[c];
-            int r = tout[c] - 1; // tout 是开区间，所以 r 是 tout[c]-1
-            int val = c;
-
-            queries_start[l].push_back({query_id, val});
-            queries_end[r].push_back({query_id, val});
-            answers.push_back(0); // 为该 query_id 占位
-            query_id++;
+            int r = tout[c] - 1;
+            
+            qs[l].push_back({qid, c});
+            qe[r].push_back({qid, c});
+            ans[qid] = 0;
+            qid++;
         }
     }
-
-    // 3. 扫描线 + 树状数组
-    BIT bit(n); // bit 维护 [0...n-1] 的值
-    ll total_k = 0;
-
-    for (int i = 0; i < n; ++i) { // i 是 DFS 序时间
-        int v = flat_tree[i]; // v 是当前节点的值
-
-        // 处理在 i 处开始的查询
-        for (const auto& q : queries_start[i]) {
-            // 查询 bit[q.val + 1 ... n-1]
-            int count_gt = bit.query_range(q.val + 1, n - 1);
-            answers[q.id] -= count_gt;
+    
+    bit.init(n);
+    ll tot = 0;
+    
+    for(int i = 0; i < n; ++i) {
+        int v = flat[i];
+        
+        for(auto q : qs[i]) {
+            int cnt = bit.qry(q.val + 1, n - 1);
+            ans[q.id] -= cnt;
         }
-
-        // 将当前节点加入 bit
+        
         bit.add(v, 1);
-
-        // 处理在 i 处结束的查询
-        for (const auto& q : queries_end[i]) {
-            // 查询 bit[q.val + 1 ... n-1]
-            int count_gt = bit.query_range(q.val + 1, n - 1);
-            answers[q.id] += count_gt;
+        
+        for(auto q : qe[i]) {
+            int cnt = bit.qry(q.val + 1, n - 1);
+            ans[q.id] += cnt;
         }
     }
-
-    // 4. 汇总 k
-    for (ll ans : answers) {
-        total_k += ans;
+    
+    for(int i = 0; i < qid; ++i) {
+        tot += ans[i];
     }
-
-    // 5. 计算 2^k mod MOD
-    cout << power(2, total_k) << "\n";
+    
+    writeln(qpow(2, tot));
 }
 
 int main() {
-    // 禁用 C++ 流同步，加快 IO
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
 
-    int c, t;
-    cin >> c >> t; // c 是样例编号，t 是数据组数
-    while (t--) {
+    freopen("dfs.in", "r", stdin);
+    freopen("dfs.out", "w", stdout);
+
+    int c = read(), t = read();
+    
+    while(t--) {
         solve();
     }
+    
     return 0;
 }
--- ccc.cpp
+++ ccc.cpp
@@ -1,3 +1,148 @@
-2025-12-31T13:10:00+08:00
-随机数:73519
-提交次数:9378
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ull unsigned long long
+#define N 100005
+
+using namespace std;
+
+int n, q;
+int a[N], b[N];
+int lza[N << 2], lzb[N << 2];
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+void pushd(int lz[], int p) {
+    if(lz[p]) {
+        lz[p << 1] ^= 1;
+        lz[p << 1 | 1] ^= 1;
+        lz[p] = 0;
+    }
+}
+
+void upd(int lz[], int p, int l, int r, int ql, int qr) {
+    if(ql <= l && r <= qr) {
+        lz[p] ^= 1;
+        return;
+    }
+    
+    pushd(lz, p);
+    
+    int mid = (l + r) >> 1;
+    if(ql <= mid) {
+        upd(lz, p << 1, l, mid, ql, qr);
+    }
+    if(qr > mid) {
+        upd(lz, p << 1 | 1, mid + 1, r, ql, qr);
+    }
+}
+
+int qry(int lz[], int p, int l, int r, int x) {
+    if(l == r) {
+        return lz[p];
+    }
+    
+    pushd(lz, p);
+    
+    int mid = (l + r) >> 1;
+    if(x <= mid) {
+        return qry(lz, p << 1, l, mid, x);
+    } else {
+        return qry(lz, p << 1 | 1, mid + 1, r, x);
+    }
+}
+
+void solve() {
+    n = read(), q = read();
+    
+    for(int i = 1; i <= n; ++i) {
+        a[i] = getchar() - '0';
+    }
+    getchar();
+    for(int i = 1; i <= n; ++i) {
+        b[i] = getchar() - '0';
+    }
+    
+    memset(lza, 0, sizeof(lza));
+    memset(lzb, 0, sizeof(lzb));
+    
+    while(q--) {
+        int op = read(), l = read(), r = read();
+        
+        if(op == 1) {
+            upd(lza, 1, 1, n, l, r);
+        }
+        else if(op == 2) {
+            upd(lzb, 1, 1, n, l, r);
+        }
+        else {
+            int m = r - l + 1;
+            ull x = 0, k = 0;
+            
+            for(int i = 0; i < m; ++i) {
+                int pos = l + i;
+                
+                int ca = a[pos] ^ qry(lza, 1, 1, n, pos);
+                int cb = b[pos] ^ qry(lzb, 1, 1, n, pos);
+                
+                if(ca) {
+                    x |= (1ULL << (m - 1 - i));
+                }
+                if(cb) {
+                    k |= (1ULL << (m - 1 - i));
+                }
+            }
+            
+            int ans = 0;
+            for(ull y = 0; ; ++y) {
+                ans = max(ans, __builtin_popcountll(x + y));
+                
+                if(y == k) {
+                    break;
+                }
+            }
+            
+            writeln(ans);
+        }
+    }
+}
+
+int main() {
+
+    freopen("sequence.in", "r", stdin);
+    freopen("sequence.out", "w", stdout);
+
+    int t = 1;
+    
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
