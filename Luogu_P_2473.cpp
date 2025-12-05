b735105880589a6b933ca7ce18c56f41cc5494cc
AMlhd <amlhdsan@gmail.com>
2025-12-05 20:49:28 +0800

    upd

--- Luogu_P_2473.cpp
+++ Luogu_P_2473.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
+
+#define N 20
+
 using namespace std;
+
+int k, n;
+int p[N];
+int s[N];
+double dp[110][80000];
 
 inline int read() {
     int x = 0, f = 1;
@@ -30,6 +38,31 @@ inline void writeln(int x) {
 }
 
 int main() {
+
+    k = read();
+    n = read();
+
+    for(int i = 1; i <= n; ++i) {
+        p[i] = read();
+        int x;
+        while(x = read()) {
+            s[i] |= (1 << (x - 1));
+        }
+    }
+
+    for(int i = 1; i <= k; ++i) {
+        for(int S = 0; S <= (1 << n) - 1; ++S) {
+            for(int j = 1; j <= n; ++j) {
+                if((s[j] & S) != s[j]) 
+                    dp[i][S] += dp[i - 1][S];
+                else 
+                    dp[i][S] += max(dp[i - 1][S], p[j] + dp[i - 1][S | 1 << j]);
+            }
+            dp[i][S] /= n * 1.0;
+        }
+    }
+
+    printf("%.6lf", dp[k][1]);
 
     return 0;
 }
