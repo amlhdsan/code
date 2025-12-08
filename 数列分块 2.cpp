19dade078f690656d9f997b72d14d4a79bdf51e3
AMlhd <amlhdsan@gmail.com>
2025-12-08 21:50:33 +0800

    upd

--- 数列分块 2.cpp
+++ 数列分块 2.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 
+#define int long long
 #define N 50010
+#define SN 250
 
 using namespace std;
 
 int n;
 int a[N];
+int sn;
+int id[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -35,16 +39,41 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-int main() {
+inline void mdf(int l, int r, int x) {
+
+}
+
+inline void qry(int l, int r, int x) {
+
+}
+
+signed main() {
 
     n = read();
+    sn = sqrt(n);
+
+    for(int i = 1; i <= n; ++i) {
+        id[i] = (i - 1) / sn + 1;
+    }
 
     for(int i = 1; i <= n; ++i) {
         a[i] = read();
     }
 
     for(int i = 1; i <= n; ++i) {
-        
+        int opt, l, r, c;
+
+        opt = read();
+        l = read();
+        r = read();
+        c = read();
+
+        if(opt == 0) {
+            mdf(l, r, c);
+        }
+        else {
+            qry(l, r, c * c);
+        }
     }
 
     return 0;
