b0c18161e2da97d7df7a88db8580e456554619f1
AMlhd <amlhdsan@gmail.com>
2025-12-08 21:32:15 +0800

    upd

--- 数列分块 1.cpp
+++ 数列分块 1.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 
+#define int long long
 #define N 50000
 #define SN 250
 
 using namespace std;
 
 int n;
+int a[N];
 int tag[SN];
+int sn;
+int id[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -36,7 +40,52 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
-int main() {
+inline void add(int l, int r, int x) {
+    for(int i = l; i <= min(id[l] * sn, r); ++i) {
+        a[i] += x;
+    }
+
+    if(id[l] != id[r]) {
+        for(int i = (id[r] - 1) * sn + 1; i <= r; ++i) {
+            a[i] += x;
+        }
+    }
+
+    for(int i = id[l] + 1; i <= id[r] - 1; ++i) {
+        tag[i] += x;
+    }
+}
+
+
+
+signed main() {
+
+    n = read();
+
+    sn = sqrt(n);
+
+    for(int i = 1; i <= n; ++i) {
+        id[i] = (i - 1) / sn + 1;
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        a[i] = read();
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        int opt, l, r, c;
+        opt = read();
+        l = read();
+        r = read();
+        c = read();
+
+        if(opt == 0) {
+            add(l, r, c);
+        }
+        else {
+            writeln(a[r] + tag[id[r]]);
+        }
+    }
 
     return 0;
 }
