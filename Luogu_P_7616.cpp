1b3907c84143f396f794cc2e5711ed0032b615b1
AMlhd <amlhdsan@gmail.com>
2025-12-11 22:15:11 +0800

    upd

--- Luogu_P_7616.cpp
+++ Luogu_P_7616.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
+
+#define N 1005
+#define ll long long
+#define MOD 1000000000
+
 using namespace std;
 
-inline int read() {
+ll n, m, a[N], b[N], ans = 1, g;
+bool flag = false;
+
+ll gcd(ll x, ll y) {
+    return y ? gcd(y, x % y) : x;
+}
+
+inline ll read() {
     int x = 0, f = 1;
     char ch = getchar();
     while (ch < '0' || ch > '9') {
@@ -15,7 +27,7 @@ inline int read() {
     return x * f;
 }
 
-inline void write(int x) {
+inline void write(ll x) {
     if (x < 0) {
         putchar('-');
         x = -x;
@@ -24,12 +36,38 @@ inline void write(int x) {
     putchar(x % 10 + '0');
 }
 
-inline void writeln(int x) {
-    write(x);
-    putchar('\n');
-}
-
 int main() {
+    n = read();
+    for(int i = 1; i <= n; ++i) {
+        a[i] = read();
+    }
+    m = read();
+    for(int i = 1; i <= m; ++i) {
+        b[i] = read();
+    }
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= m; ++j) {
+            g = gcd(a[i], b[j]);
+            if(ans * g >= MOD) {
+                flag = true;
+                ans = (ans * g) % MOD;
+            }
+            else {
+                ans *= g;
+            }
+            a[i] /= g;
+            b[j] /= g;
+        }
+    }
+    if(flag) {
+        printf("%09lld", ans);
+    }
+    else {
+        write(ans);
+    }
 
+    for(int i = 1; i <= n; ++i) {
+        
+    }
     return 0;
 }
