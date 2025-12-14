f4a1b1a6de7fe678c037836d6e24db5899e1db0a
AMlhd <amlhdsan@gmail.com>
2025-12-14 10:51:30 +0800

    upd

--- Luogu_AT_abc_153_f.cpp
+++ Luogu_AT_abc_153_f.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-#define N 400010
+#define N 200010
 #define ll long long
 #define ls (p << 1)
 #define rs (p << 1 | 1)
@@ -8,8 +8,8 @@
 
 using namespace std;
 
-inline int read() {
-    int x = 0, f = 1;
+inline ll read() {
+    ll x = 0, f = 1;
     char ch = getchar();
     while (ch < '0' || ch > '9') {
         if (ch == '-') f = -1;
@@ -36,33 +36,20 @@ inline void writeln(ll x) {
     putchar('\n');
 }
 
-int n, d, k, m, idx;
-ll ans;
-vector<int> p;
-unordered_map<int, int> mp;
+int n, m, idx;
+ll d, a, ans;
+vector<ll> p;
+map<ll, int> mp;
 
-struct point {
-    int x, num;
-    bool operator<(const point &b) const {
+struct node {
+    ll x, h;
+    bool operator<(const node &b) const {
         return x < b.x;
     }
 } arr[N];
 
-struct sec {
-    int l, r, num;
-    bool operator<(const sec &b) const {
-        if (l != b.l) return l < b.l;
-        return r < b.r;
-    }
-} s[N];
-
 ll tree[N << 2];
 ll lazy[N << 2];
-
-inline int up(int a, int b) {
-    if (a % b == 0) return a / b;
-    return a / b + 1;
-}
 
 void upd(int p) {
     tree[p] = tree[ls] + tree[rs];
@@ -90,51 +77,46 @@ void mdf(int p, int l, int r, int ql, int qr, ll k) {
     upd(p);
 }
 
-ll qry(int p, int l, int r, int ql, int qr) {
-    if (ql <= l && r <= qr) return tree[p];
+ll qry(int p, int l, int r, int pos) {
+    if (l == r) return tree[p];
     pushd(p, l, r);
-    ll sum = 0;
-    if (ql <= mid) sum += qry(ls, l, mid, ql, qr);
-    if (qr > mid) sum += qry(rs, mid + 1, r, ql, qr);
-    return sum;
+    if (pos <= mid) return qry(ls, l, mid, pos);
+    return qry(rs, mid + 1, r, pos);
 }
 
 int main() {
     n = read();
     d = read();
-    k = read();
+    a = read();
     
     for (int i = 1; i <= n; i++) {
         arr[i].x = read();
-        int h = read();
-        arr[i].num = up(h, k);
+        arr[i].h = read();
     }
     
     sort(arr + 1, arr + n + 1);
     
     for (int i = 1; i <= n; i++) {
-        p.push_back(max(1ll, (ll)arr[i].x - d));
+        p.push_back(arr[i].x);
         p.push_back(arr[i].x + d);
-        s[i] = {max(1, arr[i].x - d), arr[i].x + d, arr[i].num};
     }
     
     sort(p.begin(), p.end());
     p.erase(unique(p.begin(), p.end()), p.end());
     
     for (auto x : p) mp[x] = ++idx;
+    m = idx;
     
     for (int i = 1; i <= n; i++) {
-        s[i] = {mp[s[i].l], mp[s[i].r], s[i].num};
-        m = max(m, s[i].r);
-    }
-    
-    sort(s + 1, s + n + 1);
-    
-    for (int i = 1; i <= n; i++) {
-        ll cnt = s[i].num - qry(1, 1, m, s[i].l, s[i].r);
-        if (cnt <= 0) continue;
-        ans += cnt;
-        mdf(1, 1, m, s[i].l, s[i].r, cnt);
+        ll dmg = qry(1, 1, m, mp[arr[i].x]);
+        if (dmg >= arr[i].h) continue;
+        
+        ll need = (arr[i].h - dmg + a - 1) / a;
+        ans += need;
+        
+        int l = mp[arr[i].x];
+        int r = mp[arr[i].x + d];
+        mdf(1, 1, m, l, r, need * a);
     }
     
     writeln(ans);
