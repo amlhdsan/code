c3c2835eae3953a6d0f356013c08ff13d8d390ed
AMlhd <amlhdsan@gmail.com>
2025-12-16 20:41:49 +0800

    upd

--- Luogu_P_6100.cpp
+++ Luogu_P_6100.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 
-#define N 210
+#define M 210
+#define N 201
+#define ll long long
 
 using namespace std;
 
-int n, k;
-int c[N][N];
-int a[N][N];
-int d[N][N];
+ll fl, kt;
+ll n, k;
+ll c[M][M];
+ll a[M][M];
+ll sum[M][M];
+ll ans2 = 0, ans = 0;
+ll f[M][M], g[M][M], s[M];
 
-inline int read() {
-    int x = 0, f = 1;
+inline ll read() {
+    ll x = 0, f = 1;
     char ch = getchar();
     while (ch < '0' || ch > '9') {
         if (ch == '-') f = -1;
@@ -23,7 +28,7 @@ inline int read() {
     return x * f;
 }
 
-inline void write(int x) {
+inline void write(ll x) {
     if (x < 0) {
         putchar('-');
         x = -x;
@@ -32,50 +37,94 @@ inline void write(int x) {
     putchar(x % 10 + '0');
 }
 
-inline void writeln(int x) {
+inline void writeln(ll x) {
     write(x);
     putchar('\n');
 }
 
-inline void add(int x1, int y1, int x2, int y2, int x) {
-    c[x1][y1] += x;
-    c[x2 + 1][y1] -= x;
-    c[x1][y2 + 1] -= x;
-    c[x2 + 1][y2 + 1] += x;
-    return;
+ll cal(ll x, ll y, ll xx, ll yy) {
+    return sum[xx][yy] - sum[x - 1][yy] - sum[xx][y - 1] + sum[x - 1][y - 1];
 }
 
 int main() {
-
-    n = read();
-    k = read();
-
-    for(int i = 1; i <= n; ++i) {
-        int x1 = read(), y1 = read(), x2 = read(), y2 = read();
-        add(x1 + 1, y1 + 1, x2, y2, 1);
-    }
-
-    for(int i = 0; i <= 200; ++i) {
-        for(int j = 0; j <= 200; ++j) {
-            a[i][j] = a[i - 1][j] + a[i][j - 1] - a[i - 1][j - 1] + c[i][j];
-        }
-    }
-
-    for(int i = 0; i <= 200; ++i) {
-        for(int j = 0; j <= 200; ++j) {
-            if(a[i][j] == k) {
-                d[i][j] = -1;
-            }
-            else if(a[i][j] == j - 1) {
-                d[i][j] = 1;
-            }
-            // write(a[i][j]);
-            // putchar(' ');
-        }
-        // puts("");
-    }
-
+    n = read(); k = read();
     
+    for(int i = 1; i <= n; ++i) {
+        ll x = read(), y = read(), xx = read(), yy = read();
+        c[x + 1][y + 1]++;
+        c[xx + 1][yy + 1]++;
+        c[xx + 1][y + 1]--;
+        c[x + 1][yy + 1]--;
+    }
+
+    for(int i = 1; i < N; ++i) {
+        for(int j = 1; j < N; ++j) {
+            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + c[i][j];
+        }
+    }
+
+    for(int i = 1; i < N; ++i) {
+        for(int j = 1; j < N; ++j) {
+            if(sum[i][j] == k - 1) {
+                a[i][j] = 1;
+            }
+            else if(sum[i][j] == k) {
+                ans2++;
+                a[i][j] = -1;
+            }
+            else {
+                a[i][j] = 0;
+            }
+        }
+    }
+
+    for(int i = 1; i < N; ++i) {
+        for(int j = 1; j < N; ++j) {
+            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + a[i][j];
+        }
+    }
+
+    memset(f, -127, sizeof(f));
+    memset(g, -127, sizeof(g));
+
+    for(int i = 1; i < N; ++i) {
+        for(int j = i; j < N; ++j) {
+            ll mn = 0;
+            for(int x = 1; x < N; ++x) {
+                s[x] = s[x - 1] + cal(x, i, x, j);
+                f[x][j] = max(f[x][j], s[x] - mn);
+                mn = min(s[x], mn);
+            }
+            mn = 0;
+            for(int x = N - 1; x >= 1; --x) {
+                s[x] = s[x + 1] + cal(x, i, x, j);
+                g[x][i] = max(g[x][i], s[x] - mn);
+                mn = min(s[x], mn);
+            }
+        }
+    }
+
+    for(int i = 1; i < N; ++i) {
+        for(int j = 1; j < N; ++j) {
+            f[i][j] = max({f[i - 1][j], f[i][j - 1], f[i][j]});
+        }
+    }
+
+    for(int i = N - 1; i >= 1; --i) {
+        for(int j = N - 1; j >= 1; --j) {
+            g[i][j] = max({g[i + 1][j], g[i][j + 1], g[i][j]});
+        }
+    }
+
+    for(int i = 2; i < N; ++i) {
+        ans = max({ans, f[N - 1][i - 1] + g[1][i]});
+    }
+
+    for(int i = 2; i < N; ++i) {
+        ans = max({ans, f[i - 1][N - 1] + g[i][1]});
+    }
+
+    write(ans + ans2);
 
     return 0;
 }
