8d26ddca63866cff5cb59e69c3e1d726be5c1f9c
AMlhd <amlhdsan@gmail.com>
2025-12-20 10:19:20 +0800

    upd

--- Luogu_P_6268.cpp
+++ Luogu_P_6268.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
+
+#define N 2000010
+
 using namespace std;
 
 int n, m;
 bool iff[1010][1010];
+int nxt[N], head[N], to[N], e = 0;
 
 inline int read() {
     int x = 0, f = 1;
@@ -36,6 +40,10 @@ inline int dinic() {
 
 }
 
+inline void add_edge(int u, int v) {
+
+}
+
 int main() {
 
     n = read();
@@ -46,7 +54,18 @@ int main() {
         u = read();
         v = read();
 
+        u += 1;
+        v += 1;
+
         iff[u][v] = iff[v][u] = 1;
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= n; ++j) {
+            if(!iff[i][j]) {
+                add_edge(i, j);
+            }
+        }
     }
 
     
