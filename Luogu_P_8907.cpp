daa3cc2ca963492ad724eb28dca0591e35ae0d38
AMlhd <amlhdsan@gmail.com>
2026-02-14 23:44:59 +0800

    upd

--- Luogu_P_8907.cpp
+++ Luogu_P_8907.cpp
@@ -1,35 +1,12 @@
-/*
-const int N=2e5+7;
-int n,m;
-set<int>g[N];
-
-void Main(){
-	n=read();m=read();
-	for(int i=1;i<=m;i++){
-		int u=read(),v=read();
-		g[min(u,v)].insert(max(u,v));
-	}
-	ll ans=-m;
-	for(int i=1;i<=n;i++){
-		if(g[i].empty())continue;
-		ans+=g[i].size();
-		int u=*g[i].begin(),v=i;g[v].erase(g[v].begin());
-		if(g[u].size()<g[v].size())swap(g[u],g[v]);
-		for(auto i:g[v])g[u].insert(i);
-	}
-	cout<<ans;
-	return;
-}
-*/
 #include <bits/stdc++.h>
 
-#define N 200010
+#define N 200007
+#define ll long long
 
 using namespace std;
 
 int n, m;
-
-inline 
+set<int> g[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -45,7 +22,7 @@ inline int read() {
     return x * f;
 }
 
-inline void write(int x) {
+inline void write(ll x) {
     if (x < 0) {
         putchar('-');
         x = -x;
@@ -54,22 +31,31 @@ inline void write(int x) {
     putchar(x % 10 + '0');
 }
 
-inline void writeln(int x) {
-    write(x);
-    putchar('\n');
-}
-
 int main() {
-    
     n = read();
     m = read();
-
+    
     for(int i = 1; i <= m; ++i) {
         int u = read(), v = read();
-        add_edge(u, v);
+        g[min(u, v)].insert(max(u, v));
     }
-
-
-
+    
+    ll ans = -m;
+    
+    for(int i = 1; i <= n; ++i) {
+        if(g[i].empty()) continue;
+        
+        ans += g[i].size();
+        
+        int u = *g[i].begin(), v = i;
+        g[v].erase(g[v].begin());
+        
+        if(g[u].size() < g[v].size()) swap(g[u], g[v]);
+        
+        for(auto j : g[v]) g[u].insert(j);
+    }
+    
+    write(ans);
+    
     return 0;
 }
