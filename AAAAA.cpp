cd8c24d9a57d5a65e0ba04048f4118e20449a1ad
AMlhd <amlhdsan@gmail.com>
2026-02-20 00:16:35 +0800

    upd

---
+++ AAAAA.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+2^p - 1;
+signed main() {
+
+    int n = read();
+int ans = 0;
+    for(int i = 1; i <= n; ++i) {
+ans += read();
+    }
+
+    writeln(ans);
+
+    return 0;
+}
