33e2cac72cde27b1ed00bc10335f562b183c71e7
AMlhd <amlhdsan@gmail.com>
2026-03-07 22:15:06 +0800

    upd

--- Luogu_P_15629.cpp
+++ Luogu_P_15629.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
+
+#define N 250010
+
 using namespace std;
 
 int n;
 string str;
+bool tag[N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -33,6 +37,38 @@ inline void writeln(int x) {
 }
 
 int main() {
+
+    n = read();
+    cin >> str;
+
+    str = " " + str;
+
+    for(int i = 1; i <= n - 1; ++i) {
+        if(str[i] == str[i + 1]) {
+            tag[i] = 1;
+        }
+        if(str[i] == 'R' && str[i + 1] == 'V' || str[i] == 'V' && str[i + 1] == 'R') {
+            tag[i] = 1;
+        }
+        if(str[i] == 'B' && str[i + 1] == 'V' || str[i] == 'V' && str[i + 1] == 'B') {
+            tag[i] = 1;
+        }
+    }
+
+    int maxx = 0;
+    int nl = 0;
+
+    for(int i = 1; i <= n; ++i) {
+        if(tag[i] == 0) {
+            ++nl;
+        }
+        else {
+            maxx = max(maxx, nl);
+            nl = 0;
+        }
+    }
+
+    writeln(maxx + 1);
 
     return 0;
 }
