d4a890e596518dddf5f361daaf9bb4d60c8041de
AMlhd <amlhdsan@gmail.com>
2026-03-08 22:22:44 +0800

    upd

---
+++ Luogu_P_1375.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define int long long
+
+using namespace std;
+
+int n;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+inline int qpow(int a, int b) {
+
+}
+
+signed main() {
+
+    n = read();
+    int k1 = 1;
+    for(int i = 2; i <= n * 2; ++i) {
+        
+    }
+
+    return 0;
+}
