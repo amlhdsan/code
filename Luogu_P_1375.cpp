c7cc025844af189e88ad56df0e091bf26dd72f99
AMlhd <amlhdsan@gmail.com>
2026-03-08 22:28:17 +0800

    upd

--- Luogu_P_1375.cpp
+++ Luogu_P_1375.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int n;
+int p;
 
 inline int read() {
     int x = 0, f = 1;
@@ -41,10 +42,22 @@ inline int qpow(int a, int b) {
 signed main() {
 
     n = read();
+    p = read();
     int k1 = 1;
     for(int i = 2; i <= n * 2; ++i) {
-        
+        k1 = k1 * i % p;
     }
+    int k2 = 1;
+    for(int i = 2; i <= n; ++i) {
+        k2 = k2 * i % p;
+    }
+
+    int k2 = 1;
+    for(int i = 2; i <= n + 1; ++i) {
+        k2 = k2 * i % p;
+    }
+
+    
 
     return 0;
 }
