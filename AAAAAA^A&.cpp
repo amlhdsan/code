7e56f361624ad700687e8bf331dd8ca72100a4e8
AMlhd <amlhdsan@gmail.com>
2026-03-14 22:41:10 +0800

    upd

--- AAAAAA^A&.cpp
+++ AAAAAA^A&.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
+
+#define N 110
+
 using namespace std;
+
+int n;
+int a[N][N];
+int t[N * N];
 
 inline int read() {
     int x = 0, f = 1;
@@ -29,7 +36,38 @@ inline void writeln(int x) {
     putchar('\n');
 }
 
+inline void solve() {
+
+    memset(t, 0, sizeof t);
+    n = read();
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= n; ++j) {
+            a[i][j] = read;
+            t[a[i][j]] += 1;
+        }
+    }
+
+    int maxx = -1;
+
+    for(int i = 1; i <= N * N - 1; ++i) {
+        maxx = max(maxx, t[a[i][j]]);
+    }
+
+    if(maxx <= n * n - n) {
+        puts("YES");
+    }
+    else {
+        puts("NO");
+    }
+}
+
 int main() {
+
+    int T = read();
+
+    while(T--) {
+        solve();
+    }
 
     return 0;
 }
